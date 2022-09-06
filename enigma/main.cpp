b47a4cd2081edc994973e7ac532dfc01d1b39b5a
Gerhard Otto <gerrieotto123@gmail.com>
2022-09-06 12:53:31 +0200

    Initial Commit

---
+++ enigma/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  enigma
+//
+//  Created by Gerhard Otto on 2022/09/06.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
